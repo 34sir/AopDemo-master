538a0a12dcbaea531e64df5167bc1fc060eb6a10
chukaicheng <kaicheng.chu@sojex.cn>
2018-02-08 15:14:06 +0800

    gradle demo

---
+++ libtask/src/main/groovy/com.oil/demo/gradle/task/HelloFourTask.groovy
@@ -0,0 +1,13 @@
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+
+class HelloFourTask extends DefaultTask {
+    @Optional
+    String message = 'I am 34sir'
+
+    @TaskAction
+    def hello() {
+        println "hello world $message"
+    }
+}
